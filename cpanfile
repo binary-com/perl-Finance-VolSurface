7dcb4384c359c06c18a564b00129f58ff5da79db
Tom Molesworth <tom@binary.com>
2017-05-11 11:37:06 +0800

    Add files to git

---
+++ cpanfile
@@ -0,0 +1,11 @@
+requires 'Moose';
+requires 'Time::HiRes';
+requires 'Math::Util::CalculatedValue::Validatable';
+requires 'Date::Utility';
+requires 'Format::Util::Numbers';
+requires 'Time::Duration::Concise';
+requires 'YAML';
+
+on develop => sub {
+    requires 'Devel::Cover::Report::Kritika', '>= 0.05';
+};
---
+++ lib/Finance/VolSurface.pm
@@ -0,0 +1,201 @@
+package Finance::VolSurface;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Finance::VolSurface - 
+
+=head1 SYNOPSIS
+
+    use feature qw(say);
+    use Finance::VolSurface;
+
+=head1 DESCRIPTION
+
+
+=head2 Construction
+
+    Finance::VolSurface->new({
+        underlying    => 'frxUSDJPY',
+    });
+
+=cut
+
+use Moo;
+
+use Finance::VolSurface::Utils;
+
+=head2 for_date
+
+The date for which we want to have the volatility surface data
+
+=cut
+
+has for_date => (
+    is      => 'ro',
+    isa     => 'Maybe[Date::Utility]',
+    default => undef,
+);
+
+=head2 effective_date
+
+Surfaces roll over at 5pm NY time, so the vols of any surfaces recorded after 5pm NY but
+before GMT midnight are effectively for the next GMT day. This attribute holds this
+effective date.
+
+=cut
+
+has effective_date => (
+    is         => 'ro',
+    isa        => 'Date::Utility',
+    init_arg   => undef,
+    lazy_build => 1,
+);
+
+sub _build_effective_date {
+    my $self = shift;
+
+    return Quant::Framework::VolSurface::Utils->new->effective_date_for($self->recorded_date);
+}
+
+=head2 type
+
+Type of the surface, delta, moneyness or flat.
+
+=cut
+
+has type => (
+    is       => 'ro',
+    isa      => 'qf_surface_type',
+    required => 1,
+    init_arg => undef,
+    default  => undef,
+);
+
+
+=head2 surface
+
+Volatility surface in a hash reference.
+
+=cut
+
+has surface => (
+    is         => 'ro',
+    lazy_build => 1,
+);
+
+=head2 surface_data
+
+The original surface data.
+
+=cut
+
+has surface_data => (
+    is         => 'ro',
+    lazy_build => 1,
+);
+
+=head2 symbol
+
+The symbol of the underlying that this surface is for (e.g. frxUSDJPY)
+
+=cut
+
+has symbol => (
+    is         => 'ro',
+    lazy_build => 1,
+);
+
+
+=head2 recorded_date
+
+The date (and time) that the surface was recorded, as a Date::Utility.
+
+=cut
+
+has recorded_date => (
+    is         => 'ro',
+    isa        => 'Date::Utility',
+    lazy_build => 1,
+);
+
+
+=head2 smile_points
+
+The points across a smile.
+
+It can be delta points, moneyness points or any other points that we might have in the future.
+
+=cut
+
+has smile_points => (
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    lazy_build => 1,
+);
+
+sub _build_smile_points {
+    my $self = shift;
+
+    # Default to the point found in the first day we find
+    # in $self->surface that has a smile. As long as each smile
+    # has the same points, this works. If each smile has different
+    # points, the Validator is going to give you trouble!
+    my $surface = $self->surface;
+    my $suitable_day = first { exists $surface->{$_}->{smile} } @{$self->term_by_day};
+
+    return [sort { $a <=> $b } keys %{$surface->{$suitable_day}->{smile}}] if $suitable_day;
+    return [];
+}
+
+=head2 spread_points
+
+This will give an array-reference containing volatility spreads for first tenor which has a volatility spread (or ATM if none).
+
+=cut
+
+has spread_points => (
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    lazy_build => 1,
+);
+
+sub _build_spread_points {
+    my $self = shift;
+
+    # Default to the point found in the first day we find
+    # in $self->surface that has a volspread. As long as each volspread
+    # has the same points, this works. If each smile has different
+    # points, the Validator is going to give you trouble!
+    my $surface = $self->surface;
+    my $suitable_day = first { exists $surface->{$_}->{vol_spread} } keys %{$surface};
+
+    return [sort { $a <=> $b } keys %{$surface->{$suitable_day}{vol_spread}}] if $suitable_day;
+    return [];
+}
+
+=head2 term_by_day
+
+Get all the terms in a surface in ascending order.
+
+=cut
+
+has term_by_day => (
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    init_arg   => undef,
+    lazy_build => 1,
+);
+
+sub _build_term_by_day {
+    my $self = shift;
+
+    return [sort { $a <=> $b } keys %{$self->surface}];
+}
+
+
+1;
