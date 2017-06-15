f98854bcc58b323e0002b744ac38a18acc900cb1
Tom Molesworth <tom@binary.com>
2017-06-15 11:43:42 +0800

    Drop for_date, it appears to be unused

--- lib/Finance/VolSurface.pm
+++ lib/Finance/VolSurface.pm
@@ -160,6 +160,8 @@ use Finance::VolSurface::Utils;
 use Finance::VolSurface::Types qw(Finance_VolSurface_Type);
 use Finance::VolSurface::ExpiryConventions;
 
+=head1 ATTRIBUTES
+
 =head2 effective_date
 
 Surfaces roll over at 5pm NY time, so the vols of any surfaces recorded after 5pm NY but
@@ -180,20 +182,10 @@ sub _build_effective_date {
     return Finance::VolSurface::Utils->new->effective_date_for($self->recorded_date);
 }
 
-=head2 for_date
-
-The date for which we want to have the volatility surface data
-
-=cut
-
-has for_date => (
-    is      => 'ro',
-    default => undef,
-);
-
 =head2 recorded_date
 
-The date (and time) that the surface was recorded, as a Date::Utility.
+The date (and time) that the surface was recorded, as a L<Date::Utility>. This should
+be provided on construction.
 
 =cut
 
